133e3e546c05d3dd06841e500c64ebefa0ac504d
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-08-02 01:02:11 +0700

    docs(data mockup): created data mokup

---
+++ src/data/recommendedMenu.js
@@ -0,0 +1,32 @@
+export const recommendedMenu = [
+  {
+    id: 1,
+    name: "spaghetti bologness",
+    description:
+      "ซอสบอลโลเนสซีสำหรับสปาเก็ตตี้รสชาติอร่อยเข้มข้นกับเนื้อสับหมูดำ",
+    img: { url: "./src/assets/images/section.png", alt: "" },
+    time: "15-30นาที",
+    menuForPeople: "2ที่",
+    level: "easy",
+  },
+  {
+    id: 2,
+    name: "spaghetti bologness",
+    description:
+      "ซอสบอลโลเนสซีสำหรับสปาเก็ตตี้รสชาติอร่อยเข้มข้นกับเนื้อสับหมูดำ",
+    img: { url: "./src/assets/images/section.png", alt: "" },
+    time: "15-30นาที",
+    menuForPeople: "2ที่",
+    level: "medium",
+  },
+  {
+    id: 3,
+    name: "spaghetti bologness",
+    description:
+      "ซอสบอลโลเนสซีสำหรับสปาเก็ตตี้รสชาติอร่อยเข้มข้นกับเนื้อสับหมูดำ",
+    img: { url: "./src/assets/images/section.png", alt: "" },
+    time: "15-30นาที",
+    menuForPeople: "2ที่",
+    level: "hard",
+  },
+];
