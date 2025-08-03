635f3c519ed0afb7dca1faa02bf2d424bfb72bc7
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-08-03 16:21:02 +0700

    fix(IngredientCard): edit path image icon

--- src/components/IngredientCard.jsx
+++ src/components/IngredientCard.jsx
@@ -3,7 +3,7 @@ export const IngredientCard = ({ icon, ingredient }) => {
   return (
     <div className="relative flex gap-3 items-center p-4 bg-white w-full rounded-3xl border-1 border-brown-600 shadow-md hover:shadow-lg">
       <span className="absolute -top-1 -right-1 bg-gray-300 opacity-95 rounded-full w-fit h-fit p-1 cursor-pointer hover:scale-95">
-        <img src="./src/assets/images/x-gray.svg" alt="" />
+        <img src="/x-gray.svg" alt="" />
       </span>
       <section className="h-8">
         <img src={icon.url} alt={icon.alt} className="w-full h-full"/>
