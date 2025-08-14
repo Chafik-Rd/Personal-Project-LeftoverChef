bbf8f13afaabe42a22923941904494a47e3d6e4c
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-08-14 17:30:50 +0700

    feat(components): created back page and main background components

---
+++ src/components/BackPage.jsx
@@ -0,0 +1,9 @@
+import { ArrowLeft } from "lucide-react";
+export const BackPage = () => {
+  return (
+    <div className="flex items-center gap-2 cursor-pointer">
+        <ArrowLeft />
+        <p>ย้อนกลับ</p>
+      </div>
+  )
+}
